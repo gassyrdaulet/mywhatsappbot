57886107da16963f5248d38341e0a939ebcc3a1f
Gassyr <gassyrdaulet@gmail.com>
2022-09-22 19:17:07 +0600

    update v0.1 alpha

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const qrcode = require('qrcode-terminal')
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const axios = require('axios')
 
+const isOnProduction = true
+
 console.log("Wait, please. The client is now starting...")
 
-const client = new Client({
+const adminNumber = '77768290879'
+let isBotActive = true
+
+const config = {
     authStrategy: new LocalAuth(),
     puppeteer: {
-		args: ['--no-sandbox'],
-	}
-});
+        args: ['--no-sandbox'],
+    }
+}
+const config2 = {
+    authStrategy: new LocalAuth(),
+}
+
+const client = new Client(isOnProduction?config:config2);
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -20,17 +30,25 @@ client.on('ready', () => {
 });
 
 client.on('message', async (message) => {
+    if(message.from === adminNumber + '@c.us' && message.body === '/SwitchPower'){
+        isBotActive = !isBotActive
+        client.sendMessage(message.from, 'Бот ' + (isBotActive?'включен':'выключен'))
+        return
+    }
+
     const content = message.body
 
-    if(content === 'Как меня зовут?') {
-        client.sendMessage(message.from, message._data.notifyName)
-    }
-    else if(content === 'Здравствуйте'){
-        client.sendMessage(message.from, 'И вам Здравствуйте!')
-    }
-    else if(message.from === '77025128757@c.us' || message.from === '77768290879@c.us'){
-        const meme = await axios("https://meme-api.herokuapp.com/gimme").then(res => res.data)
-        client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+    if(isBotActive){
+        if(content === 'Привет'){
+            client.sendMessage(message.from, `Здравствуйте, ${message._data.notifyName}!`)
+        }
+        if(content === 'Погода в Астане'){
+            message.reply('Пока что недоступно, сорри')
+        }
+        else if(content === 'meme pls'&&(message.from === '77025128757@c.us' || message.from === '77768290879@c.us')){
+            const meme = await axios("https://meme-api.herokuapp.com/gimme").then(res => res.data)
+            client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+        }
     }
 });
 
